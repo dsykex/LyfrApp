67f75e62a9c07e54c497640537c16e11cf72f32c
shayla bailey <shayla bailey>
2016-11-06 17:20:59 -0500

    Setup Event Comp

--- pages/events/events.ts
+++ pages/events/events.ts
@@ -3,6 +3,10 @@ import { NavController } from 'ionic-angular';
 import {Http} from '@angular/http';
 import {Global} from '../../Global';
 
+@Component({
+    templateUrl: 'events.html'
+})
+
 export class EventComp extends Global{
     events: any;
     constructor(public http: Http, public navCtrl: NavController){
--- pages/home/home.ts
+++ pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Http} from '@angular/http';
 import {Global} from '../../Global';
+import {EventComp} from '../events/events';
 
 @Component({
   selector: 'page-home',
@@ -11,6 +12,7 @@ export class HomePage extends Global{
 
   constructor(public navCtrl: NavController, public http: Http) {
     super(http);
+    this.navCtrl.setRoot(EventComp);
   }
 
 }
